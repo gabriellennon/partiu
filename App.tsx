2b261290bdc0c30fdd9ddb2fa0f7954a1ef0278d
gabriel.lennon <gabriel.lennon@lexos.com.br>
2021-09-22 15:56:50 -0300

    rotas

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
+import { Routes } from './src/routes';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+        <Routes/>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+    colors: {
+        primary: '#4829C2',
+
+        secondary: '#4829C2',
+        pallete_logo_blue_ligth: '#83DBF2',
+        pallete_logo_green: '#05AC6D',
+        pallete_logo_green_ligth: '#48C4B4',
+
+        success: '#12A454',
+        success_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+        
+        shape: '#FFFFFF',
+        title: '#363F5F',
+        text: '#969CB2',
+        text_dark: '#000000',
+        background: '#F0F2F5'
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold'
+    }
+}
