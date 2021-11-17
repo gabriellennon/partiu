64e0e07cf6abe01322e89a989608a3fbd19d7aaa
frontend <fontend@lexos.com.br>
2021-11-17 19:20:22 -0300

    marker map

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 
 import {
@@ -7,9 +7,29 @@ import {
 } from './styles';
 
 export function Home(){
+  //Seta a localidade inicial do mapa
+  const [mapRegion, setmapRegion] = useState({
+    latitude: -15.748182,
+    longitude: -47.753353,
+    latitudeDelta: 0.00922,
+    longitudeDelta: 0.00421,
+  });
+
+  //Seta a localidade do marcador
+  const [mapRegionMarker, setmapMarker] = useState({
+    latitude: -15.748182,
+    longitude: -47.753353,
+  });
+
   return (
     <Container>
-      <MapView style={styles.map} />
+      <MapView 
+        style={styles.map} 
+        region={mapRegion}
+        showsUserLocation={true}
+      >
+        <Marker coordinate={mapRegionMarker} title='Olha eu' description="Ver como passar o local de search pra casa" />
+      </MapView>
     </Container>
   );
 }
