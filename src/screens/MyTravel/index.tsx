296660213be6b0fb4350fb95d5a06e5c2e0a94c8
frontend <gabriellennon@wizsolucoes.com.br>
2022-01-20 16:06:39 -0300

    history travels

--- src/screens/MyTravel/index.tsx
+++ src/screens/MyTravel/index.tsx
@@ -2,15 +2,25 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { BackButton } from '../../components/BackButton';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 
 import {
     Container,
     Header,
     TitleAction,
     TextAction,
+    Cotentcards,
+    TitleCards,
+    DescContent,
+    Description,
+    Text,
+    DescText,
+    CardTravel,
 } from './styles';
 
 export function MyTravel(){
+    const theme = useTheme();
     const navigation = useNavigation();
 
     function handleBack(){
@@ -26,6 +36,72 @@ export function MyTravel(){
                     <TextAction>Viagens realizadas</TextAction>
                 </TitleAction>
             </Header>
+            <Cotentcards>
+                <CardTravel>
+                    <TitleCards>Viagem de aniversário</TitleCards>
+                    <DescContent>
+                        <Description>
+                            <MaterialIcons 
+                                name="event"
+                                size={20}
+                                color={theme.colors.primary }
+                            />
+                            <Text>Data:</Text>
+                            <DescText>28/12/2021</DescText>
+                        </Description>
+                        <Description>
+                            <MaterialIcons 
+                                name="place"
+                                size={20}
+                                color={theme.colors.primary }
+                            />
+                            <Text>Destino:</Text>
+                            <DescText>Rio de Janeiro, RJ, Brasil</DescText>
+                        </Description>
+                        <Description>
+                            <MaterialIcons 
+                                name="attach-money"
+                                size={20}
+                                color={theme.colors.primary }
+                            />
+                            <Text>Total gasto::</Text>
+                            <DescText>R$ 5.000,00</DescText>
+                        </Description>
+                    </DescContent>
+                </CardTravel>
+                <CardTravel>
+                    <TitleCards>Viagem formatura</TitleCards>
+                    <DescContent>
+                        <Description>
+                            <MaterialIcons 
+                                name="event"
+                                size={20}
+                                color={theme.colors.primary }
+                            />
+                            <Text>Data:</Text>
+                            <DescText>01/06/2021</DescText>
+                        </Description>
+                        <Description>
+                            <MaterialIcons 
+                                name="place"
+                                size={20}
+                                color={theme.colors.primary }
+                            />
+                            <Text>Destino:</Text>
+                            <DescText>Arraial do Cabo, RJ, Brasil</DescText>
+                        </Description>
+                        <Description>
+                            <MaterialIcons 
+                                name="attach-money"
+                                size={20}
+                                color={theme.colors.primary }
+                            />
+                            <Text>Total gasto::</Text>
+                            <DescText>R$ 8.000,00</DescText>
+                        </Description>
+                    </DescContent>
+                </CardTravel>
+            </Cotentcards>
     </Container>
   );
 }
--- src/screens/MyTravel/styles.ts
+++ src/screens/MyTravel/styles.ts
@@ -32,3 +32,44 @@ export const TextAction = styled.Text`
     color: ${({ theme }) => theme.colors.shape};
     margin-right: ${RFPercentage(11)}px;
 `;
+
+export const Cotentcards = styled.View`
+    padding: 0 20px;
+    margin-top: 40px;
+`;
+
+export const CardTravel = styled.View`
+    background: #FFFFFF;
+    border-radius: 5px;
+    padding: 15px 25px;
+    margin: 8px 0;
+`;
+
+export const TitleCards = styled.Text`
+    font-size: ${RFValue(16)}px;
+    font-family: ${({ theme }) => theme.fonts.regular };
+    color: ${({ theme }) => theme.colors.title};
+    margin-bottom: 10px;
+`;
+
+export const DescContent = styled.View``;
+
+export const Description = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin: 4px 0;
+`;
+
+export const Text = styled.Text`
+    font-size: ${RFValue(14)}px;
+    font-family: ${({ theme }) => theme.fonts.regular };
+    color: ${({ theme }) => theme.colors.title};
+    margin-left: 3px;
+`;
+
+export const DescText = styled.Text`
+    font-size: ${RFValue(14)}px;
+    font-family: ${({ theme }) => theme.fonts.regular };
+    color: #969CB3;
+    margin-left: 4px;
+`;
