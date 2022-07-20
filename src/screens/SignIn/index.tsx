37c8e33f8e447eb69767c2be3711681e89f54228
frontend <gabriellennon@wizsolucoes.com.br>
2022-07-19 22:46:49 -0300

    adjustments steps pedido recebido

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Alert, StatusBar } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import AppleSvg from '../../assets/icons/apple_icon.svg';
@@ -23,6 +24,19 @@ export function SignIn(){
   const navigation = useNavigation();
 
   async function handlesignInWithGoogle(){
+    const CLIENT_ID = '1046876587992-f72sgdp8jd3324o9v0tqfir61mobkii6.apps.googleusercontent.com';
+    const REDIRECT_URI = 'https://auth.expo.io/@gabriellennon/partiu';
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('profile email');
+
+
+    //Pegando o Token de autenticação
+    //Get authentication token
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
+    //Iniciando fluxo de autenticacao
+    //Start authentication
+    const response = await AuthSession.startAsync({ authUrl });
+    console.log(response);
     navigation.navigate('Inicio' as never, {} as never)
     // try {
     //     await signInWithGoogle();
